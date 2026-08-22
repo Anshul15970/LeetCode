b51235e30e48ea49d64223f1550c044ac3d017d0
Anshul15970 <anshulgurjarag15@gmail.com>
2026-08-22 23:06:36 +0530

    Time: 3 ms (38.79%), Space: 14.1 MB (65.52%) - LeetHub

---
+++ LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+       int curr = 1;
+       vector<int> ans;
+       while(ans.size() < n){
+            ans.push_back(curr);
+            if(curr * 10 <= n){curr *= 10;}
+            else{
+                while(curr%10 == 9 || curr + 1 > n){curr /= 10;}
+                curr++;
+            }
+       } 
+       return ans;
+    }
+};
