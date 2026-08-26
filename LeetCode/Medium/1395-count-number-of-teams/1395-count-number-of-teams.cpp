2550c294d5d7326b3ddff961e1aa25acd30a06dd
Anshul15970 <anshulgurjarag15@gmail.com>
2026-08-26 19:00:36 +0530

    Time: 2944 ms (5.04%), Space: 550.5 MB (5.04%) - LeetHub

--- LeetCode/Medium/1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ LeetCode/Medium/1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    vector<vector<vector<int>>> dp;
+
+    int solve(int idx,int prev,int left,vector<int> &rating,bool increasing){
+        if(left == 0){return 1;}
+        if(idx == rating.size()){return 0;}
+        if(dp[idx][prev+1][left] != -1){return dp[idx][prev+1][left];}
+        int ans = solve(idx+1,prev,left,rating,increasing);
+        if(prev == -1 || (increasing && rating[idx] > rating[prev]) || 
+           (!increasing && rating[idx] < rating[prev])){
+            ans += solve(idx+1,idx,left-1,rating,increasing);
+           }
+        return dp[idx][prev+1][left] = ans;
+    }
+
     int numTeams(vector<int>& rating) {
         int n = rating.size();
         int ans = 0;
-        for(int i = 1;i<n-1;i++){
-            int cntl1 = 0,cntl2 = 0,cntr1=0,cntr2 = 0;
-            for(int j = 0;j<i;j++){
-                if(rating[i] < rating[j]){cntl1++;}
-                else if(rating[i] > rating[j]){cntl2++;}
-            }
-            for(int j = i+1;j<n;j++){
-                if(rating[i] > rating[j]){cntr1++;}
-                else if(rating[i] < rating[j]){cntr2++;}
-            }
-            ans += (cntl1 * cntr1) + (cntl2 * cntr2);
-        }
+        dp.assign(n,vector<vector<int>>(n+1, vector<int>(4,-1)));
+        ans += solve(0,-1,3,rating,true);
+        dp.assign(n,vector<vector<int>>(n+1, vector<int>(4,-1)));
+        ans += solve(0,-1,3,rating,false);
         return ans;
     }
 };
