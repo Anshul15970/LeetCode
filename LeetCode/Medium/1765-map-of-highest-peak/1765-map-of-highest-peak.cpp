286efe9ea781bba497c6cf10f9e6bf6afe3769ef
Anshul15970 <anshulgurjarag15@gmail.com>
2026-09-04 16:11:36 +0530

    Time: 322 ms (6.28%), Space: 201.7 MB (6.11%) - LeetHub

---
+++ LeetCode/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& M) {
+        int n = M.size(), m = M[0].size();
+        queue<vector<int>> q;
+        vector<vector<int>> ans(n, vector<int> (m,-1));
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(M[i][j] == 1){q.push({i,j}); ans[i][j] = 0;}}}
+        while(q.size()){
+            int i = q.front()[0],j = q.front()[1];
+            q.pop();
+            if(i-1>=0 && ans[i-1][j] == -1){q.push({i-1,j}); ans[i-1][j]=ans[i][j]+1;}
+            if(i+1<n && ans[i+1][j] == -1){q.push({i+1,j}); ans[i+1][j]=ans[i][j]+1;}
+            if(j-1>=0 && ans[i][j-1] == -1){q.push({i,j-1}); ans[i][j-1]=ans[i][j]+1;}
+            if(j+1<m && ans[i][j+1] == -1){q.push({i,j+1}); ans[i][j+1]=ans[i][j]+1;}
+        }
+        return ans;
+    }
+};
