b592494817372315c6d77c4681878faf679250a6
Anshul15970 <anshulgurjarag15@gmail.com>
2026-09-05 11:18:51 +0530

    Time: 114 ms (31.42%), Space: 192.1 MB (8.57%) - LeetHub

---
+++ LeetCode/Medium/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<int,vector<pair<int,int>>> adj;
+    void dfs(int i,int &minval,vector<int> &vis){
+        vis[i] = 1;
+        for(auto [neighbour,val] : adj[i]){
+            minval = min(minval,val);
+            if(!vis[neighbour]){
+                dfs(neighbour,minval,vis);
+            }
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        for(auto &i : roads){
+            int u = i[0], v = i[1], val = i[2];
+            adj[u].push_back({v,val});
+            adj[v].push_back({u,val});
+        }
+        int minval = INT_MAX;
+        vector<int> vis(n+1,0);
+        dfs(1,minval,vis);
+        return minval == INT_MAX?-1:minval;
+    }
+};
