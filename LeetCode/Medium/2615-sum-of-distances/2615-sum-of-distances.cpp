f188a0a7121dc40dcd435d1b3d38570df1508033
Anshul15970 <anshulgurjarag15@gmail.com>
2026-09-17 12:16:33 +0530

    Time: 81 ms (40.82%), Space: 141.5 MB (6.8%) - LeetHub

--- LeetCode/Medium/2615-sum-of-distances/2615-sum-of-distances.cpp
+++ LeetCode/Medium/2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
     vector<long long> distance(vector<int>& nums) {
+        unordered_map<int,vector<long long>> m;
         int n = nums.size();
-
-        vector<long long> ans(n, 0);
-
- 
-        unordered_map<int, pair<long long, long long>> mp;
-
-      
-        for (int i = 0; i < n; i++) {
-            int x = nums[i];
-
-            long long count = mp[x].first;
-            long long sum = mp[x].second;
-
-         
-            ans[i] += (long long)i * count - sum;
-
-    
-            mp[x].first++;
-            mp[x].second += i;
+        vector<long long> ans(n,0);
+        for(int i = 0;i<n;i++){m[nums[i]].push_back(i);}
+        for(auto &i : m){
+            vector<long long> arr = i.second;
+            int s = arr.size();
+            if(s == 1){continue;}
+            vector<long long> prefix(s,0);
+            prefix[0] = arr[0];
+            for(int j = 1;j<s;j++){prefix[j] = prefix[j-1]+arr[j];}
+            for(int j = 0;j<s;j++){
+                long long a = (((long long)prefix[s-1]-prefix[j]) - (s-(j+1))*arr[j]) + 
+                (j>0?(long long)j*arr[j] - prefix[j-1]:0);
+                ans[arr[j]] = a;
+            }
         }
-
-   
-        mp.clear();
-
-
-        for (int i = n - 1; i >= 0; i--) {
-            int x = nums[i];
-
-            long long count = mp[x].first;
-            long long sum = mp[x].second;
-
-            ans[i] += sum - (long long)i * count;
-
-
-            mp[x].first++;
-            mp[x].second += i;
-        }
-
         return ans;
     }
 };
