5f7ff55526e888ec7e622ff5bb011100dee66288
Anshul15970 <anshulgurjarag15@gmail.com>
2026-09-26 23:21:39 +0530

    Time: 10 ms (37.69%), Space: 44 MB (70%) - LeetHub

---
+++ LeetCode/Medium/0481-magical-string/0481-magical-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int magicalString(int n) {
+        if(n == 0){return 0;}
+        if(n <= 3){return 1;}
+        int [] arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 2;
+        int countOnes = 1;
+        int readPointer = 2;
+        int writePointer = 3;
+        while(writePointer < n){
+            int nextGrpsize = arr[readPointer];
+            int nextChar = arr[writePointer-1]==1?2:1;
+            for(int i = 0;i<nextGrpsize;i++){
+                if(writePointer<n){
+                    arr[writePointer]=nextChar;
+                    if(nextChar==1){
+                        countOnes++;
+                    }
+                    writePointer++;
+                }
+            }
+            readPointer++;
+        }
+        return countOnes;
+    }
+}
